5dc5db7b60a70e6e53b21aa7d4f1a3a89b4c8f72
jpengstrom <jpengst@gmail.com>
2020-04-29 16:01:09 -0700

    added pagination. 5 entries per page

--- app/controllers/flexible_metadata/profiles_controller.rb
+++ app/controllers/flexible_metadata/profiles_controller.rb
@@ -17,7 +17,8 @@ module FlexibleMetadata
       add_breadcrumbs
       @flexible_metadata_profiles = FlexibleMetadata::Profile.all
 
-      @profiles = @flexible_metadata_profiles.order("updated_at DESC").page(params[:page])
+      @profiles = @flexible_metadata_profiles.page(params[:profile_entries_page])
+    
     end
 
     # GET /flexible_metadata_profiles/1
