9d8b2399e051ed7f753e36084c4ac10505b5e718
Rob Kaufman <rob@notch8.com>
2020-06-25 02:43:05 -0700

    make solrdocument and catalog controller dynamic w/o reloading the application

--- app/models/concerns/flexible_metadata/dynamic_solr_document.rb
+++ app/models/concerns/flexible_metadata/dynamic_solr_document.rb
@@ -5,22 +5,32 @@ module FlexibleMetadata
     extend ActiveSupport::Concern
 
     included do
-      # override (from Hyrax 2.5.0) - setup the solr attributes dynamically
-      # Gather all properties from the latest profile and setup the attributes
-      # The SolrDocument is independent of the Model and Context, hence we use
-      # profile directly.
-      profile = FlexibleMetadata::Profile.current_version
-      unless profile.blank?
-        profile.properties.each do |prop|
-          attribute(
-            prop.name,
-            # if the property is singular, make it so
-            prop.cardinality_maximum == 1 ? Hyrax::SolrDocument::Metadata::Solr::String : Hyrax::SolrDocument::Metadata::Solr::Array,
-            solr_name(prop.name.to_s)
-          )
+      attribute :dynamic_schema, Hyrax::SolrDocument::Metadata::Solr::String, solr_name(:dynamic_schema)
+    end
+
+    class_methods do
+      def load_flexible_metadata
+        # override (from Hyrax 2.5.0) - setup the solr attributes dynamically
+        # Gather all properties from the latest profile and setup the attributes
+        # The SolrDocument is independent of the Model and Context, hence we use
+        # profile directly.
+        profile = FlexibleMetadata::Profile.current_version
+        unless profile.blank?
+          profile.properties.each do |prop|
+            attribute(
+              prop.name,
+              # if the property is singular, make it so
+              prop.cardinality_maximum == 1 ? Hyrax::SolrDocument::Metadata::Solr::String : Hyrax::SolrDocument::Metadata::Solr::Array,
+              solr_name(prop.name.to_s)
+            )
+          end
         end
       end
-      attribute :dynamic_schema, Hyrax::SolrDocument::Metadata::Solr::String, solr_name(:dynamic_schema)
+    end
+
+    def initialize(source_doc = {}, response = nil)
+      self.class.load_flexible_metadata
+      super(source_doc, response)
     end
   end
 end
