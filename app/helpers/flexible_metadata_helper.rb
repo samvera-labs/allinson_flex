65ca970d9be0522fa9abe4d3a111cab7bfaebb6b
Rob Kaufman <rob@notch8.com>
2020-07-05 00:51:18 -0700

    add admin set to work type dialog

--- app/helpers/flexible_metadata_helper.rb
+++ app/helpers/flexible_metadata_helper.rb
@@ -26,6 +26,14 @@ module FlexibleMetadataHelper
     )
   end
 
+  # borrowd from batch-importer https://github.com/samvera-labs/hyrax-batch_ingest/blob/master/app/controllers/hyrax/batch_ingest/batches_controller.rb
+  def available_admin_sets
+    # Restrict available_admin_sets to only those current user can desposit to.
+    @available_admin_sets ||= Hyrax::Collections::PermissionsService.source_ids_for_deposit(ability: current_ability, source_type: 'admin_set').map do |admin_set_id|
+      [AdminSet.find(admin_set_id).title.first, admin_set_id]
+    end
+  end
+
   def flash_messages
     flash.map do |type, text|
       { id: text.object_id, type: type, text: text }
