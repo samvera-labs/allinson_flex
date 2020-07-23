dcc008a01b71b84704dfadb499314c3b0669e064
JP Engstrom <jpengstrom@MacBook-Pro.local>
2020-07-23 10:04:01 -0700

    m3 index typo edit

--- db/migrate/20200720162402_rename_m3_profile_properties.rb
+++ db/migrate/20200720162402_rename_m3_profile_properties.rb
@@ -6,10 +6,10 @@ class RenameM3ProfileProperties < ActiveRecord::Migration[5.1]
 
     remove_index :flexible_metadata_available_properties, :m3_profile_property_id
     rename_column :flexible_metadata_available_properties, :m3_profile_property_id, :flexible_metadata_profile_property_id
-    add_index :flexible_metadata_available_properties, :m3_profile_property_id, name: :flexible_metadata_available_properties_property_id
+    add_index :flexible_metadata_available_properties, :flexible_metadata_profile_property_id, name: :flexible_metadata_available_properties_property_id
 
     remove_index :m3_profile_texts, :m3_profile_property_id
     rename_column :m3_profile_texts, :m3_profile_property_id, :flexible_metadata_profile_property_id
-    add_index :m3_profile_texts, :m3_profile_property_id, name: :flexible_metadata_profile_texts_on_profile_property_id
+    add_index :m3_profile_texts, :flexible_metadata_profile_property_id, name: :flexible_metadata_profile_texts_on_profile_property_id
   end
 end
