7c36a2a347ddfd26ccd9d325b0841ea38f3f6c90
Shana <shana.lavina.moore@gmail.com>
2022-07-21 13:16:54 -0700

    prevent failing migrations due to re running references

--- db/migrate/20191009111324_add_available_on_to_allinson_flex_profile_available_property.rb
+++ db/migrate/20191009111324_add_available_on_to_allinson_flex_profile_available_property.rb
@@ -1,5 +1,5 @@
 class AddAvailableOnToAllinsonFlexProfileAvailableProperty < ActiveRecord::Migration[5.1]
   def change
-    add_reference :allinson_flex_profile_available_properties, :available_on, polymorphic: true, index: { name: 'index_allinson_flex_profile_properties_available_on' }
+    add_reference :allinson_flex_profile_available_properties, :available_on, polymorphic: true, index: { name: 'index_allinson_flex_profile_properties_available_on' } unless column_exists?(:allinson_flex_profile_available_properties, :available_on_type)
   end
 end
