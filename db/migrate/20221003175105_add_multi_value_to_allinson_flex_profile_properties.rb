ba7a93dd2110551f6120a8b1778bec7ea36d3041
Shana <shana.lavina.moore@gmail.com>
2022-10-10 11:33:46 -0700

    i154- separate migrations

--- db/migrate/20221003175105_add_multi_value_to_allinson_flex_profile_properties.rb
+++ db/migrate/20221003175105_add_multi_value_to_allinson_flex_profile_properties.rb
@@ -1,6 +1,5 @@
 class AddMultiValueToAllinsonFlexProfileProperties < ActiveRecord::Migration[5.2]
   def change
     add_column :allinson_flex_profile_properties, :multi_value, :boolean, default: false unless column_exists?(:allinson_flex_profile_available_properties, :multi_value)
-    add_column :allinson_flex_profile_properties, :requirement, :string, default: "optional", null: false unless column_exists?(:allinson_flex_profile_available_properties, :requirement)
   end
 end
