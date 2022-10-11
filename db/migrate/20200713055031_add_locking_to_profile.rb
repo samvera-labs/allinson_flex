2054cfbfe69437be5c1ebde3a85b78d0beddd430
Shana <shana.lavina.moore@gmail.com>
2022-10-11 14:22:11 -0700

    i154 - separate migrations

--- db/migrate/20200713055031_add_locking_to_profile.rb
+++ db/migrate/20200713055031_add_locking_to_profile.rb
@@ -1,6 +1,5 @@
 class AddLockingToProfile < ActiveRecord::Migration[5.1]
   def change
     add_column :allinson_flex_profiles, :locked_at, :datetime unless column_exists?(:allinson_flex_profiles, :locked_at)
-    add_column :allinson_flex_profiles, :locked_by_id, :integer unless column_exists?(:allinson_flex_profiles, :locked_by_id)
   end
 end
