741a2d62f252d9b0cb774970535c85454b193376
Shana Moore <shana.lavina.moore@gmail.com>
2022-10-14 13:17:56 -0700

    I212 add admin only to flex hotfix (#101)
    * i212 - modify dynamic catalog behavior with admin_only
    
    * i212 - add admin_only indexing display modifier
    
    * i212 - add NoAllinsonFlexSchemaError
    
    * undo change not related to implementing admin-only
    
    * hot fix - correct reference to the wrong table name
    
    * hot fix - correct reference to the wrong table name

--- db/migrate/20221010181901_add_requirement_to_allinson_flex_profile_properties.rb
+++ db/migrate/20221010181901_add_requirement_to_allinson_flex_profile_properties.rb
@@ -1,5 +1,5 @@
 class AddRequirementToAllinsonFlexProfileProperties < ActiveRecord::Migration[5.2]
   def change
-    add_column :allinson_flex_profile_properties, :requirement, :string, default: "optional", null: false unless column_exists?(:allinson_flex_profile_available_properties, :requirement)
+    add_column :allinson_flex_profile_properties, :requirement, :string, default: "optional", null: false unless column_exists?(:allinson_flex_profile_properties, :requirement)
   end
 end
