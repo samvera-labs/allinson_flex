11d6a10767740165aa687993d83ecd0b52045fa7
LaRita Robinson <larita@scientist.com>
2022-11-03 12:07:48 -0400

    Clarify note on ProfileClass model

--- app/models/allinson_flex/profile_class.rb
+++ app/models/allinson_flex/profile_class.rb
@@ -4,6 +4,8 @@ module AllinsonFlex
   class ProfileClass < ApplicationRecord
     # @todo any before_destroy validation?
     # many to many relationship between class and context
+    #   Note: this seems incorrect, as has_and_belongs_to_many is not defined on both models, 
+    #   and additionally there is a defined table, ProfileClassContext as the intersection of these two classes.
     has_and_belongs_to_many :profile_contexts # this seems incorrect??
     has_many :available_properties, as: :available_on, class_name: 'AllinsonFlex::ProfileAvailableProperty', dependent: :destroy
     has_many :properties, through: :available_properties, source: :profile_property
